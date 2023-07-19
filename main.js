1532bb2ded7a54b058fc1c760c08ee1be464a297
Gabriel Carballo <133804940+gabrielcarballo12@users.noreply.github.com>
2023-07-19 16:25:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let oferta = prompt ("te gustaria recibir nuestras ofertas?")
+
+if ("Si"){
+    
+}else ("no")
+
+
+let nombre = prompt("Ingresa tu nombre")
+console.log(`Tu nombre es ${nombre}`);
+alert(`Tu nombre es ${nombre}`)
+
+let apellido = prompt(`Muchas gracias ${nombre}. Ahora podrias decirme tu Apellido?`)
+console.log(`Tu apellido es ${apellido}`);
+alert (`Tu apellido es ${apellido}`)
+
+alert(`Muchas gracias y bienvenido/a ${nombre} ${apellido}`)
