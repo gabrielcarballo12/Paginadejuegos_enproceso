f471adfcac42cd2589aacd7abd62048639d37426
Gabriel Carballo <133804940+gabrielcarballo12@users.noreply.github.com>
2023-07-19 18:32:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-let oferta = prompt ("te gustaria recibir nuestras ofertas?")
 
-if ("Si"){
-    
-}else ("no")
+let oferta = prompt("¿Te gustaría recibir nuestras ofertas?").toLocaleLowerCase();
 
+if (oferta === "si") {
+  alert("Gracias. Ahora le pediremos unos datos!");
 
-let nombre = prompt("Ingresa tu nombre")
-console.log(`Tu nombre es ${nombre}`);
-alert(`Tu nombre es ${nombre}`)
+  let nombre = prompt("Ingresa tu nombre");
+  console.log(`Tu nombre es ${nombre}`);
 
-let apellido = prompt(`Muchas gracias ${nombre}. Ahora podrias decirme tu Apellido?`)
-console.log(`Tu apellido es ${apellido}`);
-alert (`Tu apellido es ${apellido}`)
+  let apellido = prompt(`Muchas gracias ${nombre}. Ahora podrías decirme tu Apellido?`);
+  console.log(`Tu apellido es ${apellido}`);
 
-alert(`Muchas gracias y bienvenido/a ${nombre} ${apellido}`)
+  alert(`Muchas gracias y bienvenido/a ${nombre} ${apellido}`);
+} else {
+  alert("Gracias y bienvenido/a");
+}
